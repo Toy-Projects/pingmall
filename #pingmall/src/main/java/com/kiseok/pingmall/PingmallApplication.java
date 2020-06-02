9884cda73683423d76c032e5a705c7089c84c94d
kiseok, Yang <rltjr219@gmail.com>
2020-06-02 16:43:03 +0900

    first commit

---
+++ #pingmall/src/main/java/com/kiseok/pingmall/PingmallApplication.java
@@ -0,0 +1,13 @@
+package com.kiseok.pingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingmallApplication.class, args);
+    }
+
+}
---
+++ #pingmall/src/test/java/com/kiseok/pingmall/PingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiseok.pingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
